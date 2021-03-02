b2477d8d7a0c302d27c7605948527ec8750da10c
arturo <arturomazzoni@gmail.com>
2021-03-02 16:05:46 +0100

    commit

--- index-php.php
+++ index-php.php
@@ -1,37 +1,38 @@
 <?php
   $array = [
     [
+      "title" => "back to black",
+      "artist" => "amy winehouse",
+      "cover" => "AmyWinehouse_BackToBlack.jpg"
+    ],
+    [
       "title" => "in utero",
       "artist" => "nirvana",
-      "cover" => ""
+      "cover" => "inUteroNirvana.jpg"
     ],
     [
       "title" => "nevermind",
       "artist" => "nirvana",
-      "cover" => ""
-    ],
-    [
-      "title" => "back to black",
-      "artist" => "amy winehouse",
-      "cover" => ""
+      "cover" => "NirvanaNevermind.jpg"
     ],
     [
       "title" => "bollicine",
       "artist" => "vasco",
-      "cover" => ""
+      "cover" => "BollicineVasco.jpg"
     ],
     [
       "title" => "indestructible",
       "artist" => "rancid",
-      "cover" => ""
+      "cover" => "IndestructibleRancid.jpg"
     ],
     [
       "title" => "And out come the wolves",
       "artist" => "rancid",
-      "cover" => ""
+      "cover" => "RancidAndOutCometheWolves.jpg"
     ]
   ];
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -44,9 +45,25 @@
 </head>
 <body>
 
-  <?php foreach($array as $item){ ?>
-          <h2> <?php echo $item ?> </h2>
-  <?php } ?>
+  <header>
+    <img src="img/spotifyLogo.jpg" alt="logo">
+  </header>
+
+  <main>
+    <div class="container">
+      <div class="box">
+        <?php foreach($array as $item){ ?>
+                <h2> <?php echo $item["title"] ?> </h2>
+                <h4> <?php echo $item["artist"] ?> </h4>
+                <img src="img/<?php echo $item["cover"] ?>" alt="cover">
+        <?php } ?>
+      </div>
+    </div>
+  </main>
+
+
+  <footer></footer>
+
 
   <script src="dist/script.js" charset="utf-8"></script>
 </body>
